28c921aec525af1fb1553605dfe48cae1369ba90
Diego <dcoelhocarneiro@gmail.com>
2025-02-03 21:38:57 -0300

    renomeação das pastas e novos projetos sobre generics

---
+++ generics4/src/model/entities/Circle.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+public class Circle implements Shape {
+
+    public double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+}
