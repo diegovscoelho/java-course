6045d6415ac583cf016e86436d2a4bd93b942027
Diego <dcoelhocarneiro@gmail.com>
2025-02-10 20:48:04 -0300

    lambda 2

--- lambda1/src/application/Main.java
+++ lambda1/src/application/Main.java
@@ -1,27 +1,21 @@
-package application;
+import entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-
-import entities.Product;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
 
+    public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("TV", 900.00));
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
-
-        Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        list.sort(comp);
-
-        for (Product p : list) {
-            System.out.println(p);
-        }
+        list.sort(Main::compareProducts);
+        list.forEach(System.out::println);
     }
 }
